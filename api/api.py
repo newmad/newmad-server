362296f80cd6275dad0c2a110010a74bdb9fc727
Jaeho Cho <jaeho8032@gmail.com>
2018-09-01 16:59:09 +0900

    [#1] 날씨 받아오는 api 만들기
    - 기본 날씨 api 구현

---
+++ api/api.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_restful import Resource, Api
+import os
+import requests
+
+API_KEY = os.environ.get('API_KEY', None)
+
+app = Flask(__name__)
+api = Api(app)
+
+class WeatherID(Resource):
+    def post(self):
+        try:
+            url = 'https://api.openweathermap.org/data/2.5/weather?q=Jeju,KR&appid=' + API_KEY
+            response = requests.get(url)
+            return response.text
+
+        except Exception as e:
+            return {'error': str(e)}
+
+api.add_resource(WeatherID, '/weather')
+
+'''
+class WeatherGEO(Resource):
+    def post(self):
+        try:
+            pass
+        except Exception as e:
+            return {'error': str(e)}
+'''
